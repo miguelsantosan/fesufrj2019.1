4dee503133f6c2495b1006a48b52e8c921daf47b
miguelsantosan <12385420+miguelsantosan@users.noreply.github.com>
2019-03-22 21:12:19 -0300

    Add files via upload

--- VoceAluga_Grupo9/src/com/company/Carro.java
+++ VoceAluga_Grupo9/src/com/company/Carro.java
@@ -1,4 +1,18 @@
 package com.company;
 
 public class Carro {
+    private Marca marca;
+
+    public Carro(Marca marcaDoCarro){
+        this.marca = marcaDoCarro;
+    }
+
+    public Marca getMarcaDoCarro() {
+        return this.marca;
+    }
+
+    public String getNomeDaMarcaDoCarro() {
+        return this.marca.nome;
+    }
+
 }
