73651b6a58548c0a1a852756de7136ff9b411451
RenanIOliveira <renanioliveira@dcc.ufrj.br>
2019-05-08 09:28:20 -0300

    Corrgido um bug em que ao alterar o cadastro de um cliente era permitida a inserção de um CPF ou passaporte vazio

---
+++ VoceAluga/src/model/dao/CadastroHabilitacao.java
@@ -0,0 +1,14 @@
+package model.dao;
+
+public class CadastroHabilitacao {
+	
+	public boolean buscarHabilitacaoPorCPF(String CPF){
+		
+		return true;
+	}
+	public boolean buscarHabilitacaoPorNumeroDeRegistro(String NumeroDeRegistro){
+		
+		return true;
+	}
+
+}
